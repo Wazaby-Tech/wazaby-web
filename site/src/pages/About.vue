9017c138844b56413fa3d1a1a44e48027c33af1e
Michael Holloway <michael.a.holloway@outlook.com>
2020-06-20 21:52:39 -0700

    Update social media icons

--- site/src/pages/About.vue
+++ site/src/pages/About.vue
@@ -2,23 +2,33 @@
   <Layout>
    <h1>About</h1>
         <h2>History</h2>
-        <p>Founded in 2020, Wazaby Tech is a technology consulting company that bridges the divide between technology and the companies it serves. Technology should work for its companies, not make companies bow to its whim. Wazaby strives to deliver technology that works for you.</p>
+        <p>Founded in 2020, Wazaby Tech is a technology consulting company that bridges the divide between technology and the companies it serves. Technology should work for its companies, not make companies bow to its whim. Wazaby strives to deliver technology that works for you.
+        </p>
+        <p>
+        <a href="https://www.twitter.com/wazabytech">
+          <g-image alt="Twitter" src="~/assets/images/twitter.svg" width="20" />
+        </a>
+        &nbsp;&nbsp;&nbsp;
+        <a href="https://www.instagramcom/wazabytech">
+          <g-image alt="Instagram" src="~/assets/images/instagram.svg" width="20" />
+        </a>
+        </p>
         <h2>Founder</h2>
         <p>
             Michael Holloway is an experienced software engineer who has developed solutions for a variety of companies spanning government, entertainment, food, industrial and education industries. He has over 10 years experience working and specializing in Microsoft technologies.
         </p>
         <p>
           <a href="https://www.github.com/mholloway24">
-            <g-image alt="Github" src="~/assets/images/github.svg" width="35" />
+            <g-image alt="Github" src="~/assets/images/github.svg" width="20" />
           </a>&nbsp;&nbsp;&nbsp;
           <a href="https://www.twitter.com/wazabydev">
-            <g-image alt="Github" src="~/assets/images/twitter.svg" width="35" />
+            <g-image alt="Github" src="~/assets/images/twitter.svg" width="20" />
           </a>&nbsp;&nbsp;&nbsp;
           <a href="https://www.linkedin.com/in/michael-holloway-5bb15b77">
-            <g-image alt="Github" src="~/assets/images/linkedin.svg" width="35" />
+            <g-image alt="Github" src="~/assets/images/linkedin.svg" width="20" />
           </a>&nbsp;&nbsp;&nbsp;
           <a href="https://www.instagram.com/mholloway24">
-            <g-image alt="Github" src="~/assets/images/instagram.svg" width="35" />
+            <g-image alt="Github" src="~/assets/images/instagram.svg" width="20" />
           </a>
         </p>
   </Layout>
