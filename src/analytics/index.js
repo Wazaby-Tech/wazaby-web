a7473647688c2b220c6c181c5f551fa81afab846
Mike Holloway <michael.a.holloway@outlook.com>
2021-08-05 08:06:36 -0700

    Add Analytics key

--- src/analytics/index.js
+++ src/analytics/index.js
@@ -2,7 +2,7 @@ import ReactGA from 'react-ga';
 
 export const initGA = () => {
   console.log('GA init');
-  ReactGA.initialize('UA-xxxxxxxxx-1');
+  ReactGA.initialize('G-K2J3H6TG91');
 };
 
 export const logPageView = () => {
