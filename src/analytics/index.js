8dd33fa51edc0cb6ef2f8765535ca4b72f4e19a7
Mike Holloway <michael.a.holloway@outlook.com>
2021-08-15 20:55:35 -0700

    Update Google Analytics key

--- src/analytics/index.js
+++ src/analytics/index.js
@@ -2,7 +2,7 @@ import ReactGA from 'react-ga';
 
 export const initGA = () => {
   console.log('GA init');
-  ReactGA.initialize('G-K2J3H6TG91');
+  ReactGA.initialize('UA-180852644-1');
 };
 
 export const logPageView = () => {
